0a84525b3007907678bd77bda542d8362ea16009
Luka Simic <luka.simic51@gmail.com>
2019-03-17 13:20:00 +0100

    minor naming change

--- __init__.py
+++ __init__.py
@@ -1,6 +1,5 @@
 import bpy
-from . import ui
-from . import GenerateSection
+from . import UI
 from . import Generator
 
 
@@ -20,16 +19,16 @@ bl_info = {
 
 
 def register():
-    bpy.utils.register_class(ui.PBGPropertyGroup)
-    bpy.types.Scene.PBGPropertyGroup = bpy.props.PointerProperty(type=ui.PBGPropertyGroup)
-    bpy.utils.register_class(ui.PBGToolbarGeneralPanel)
-    bpy.utils.register_class(ui.PBGToolbarSettingsPanel)
+    bpy.utils.register_class(UI.PBGPropertyGroup)
+    bpy.types.Scene.PBGPropertyGroup = bpy.props.PointerProperty(type=UI.PBGPropertyGroup)
+    bpy.utils.register_class(UI.PBGToolbarGeneralPanel)
+    bpy.utils.register_class(UI.PBGToolbarSettingsPanel)
     bpy.utils.register_class(Generator.Generator)
 
 
 def unregister():
     del bpy.types.Scene.PBGPropertyGroup
-    bpy.utils.unregister_class(ui.PBGPropertyGroup)
-    bpy.utils.unregister_class(ui.PBGToolbarGeneralPanel)
-    bpy.utils.unregister_class(ui.PBGToolbarSettingsPanel)
+    bpy.utils.unregister_class(UI.PBGPropertyGroup)
+    bpy.utils.unregister_class(UI.PBGToolbarGeneralPanel)
+    bpy.utils.unregister_class(UI.PBGToolbarSettingsPanel)
     bpy.utils.unregister_class(Generator.Generator)
